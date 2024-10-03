8434e617402ecb4a512f8d0d9c8afd34d0bbc149
Taqi Raza <taqi.raza@pitb.gov.pk>
2024-10-03 12:40:18 +0500

    Update recent-projects.component.ts

--- src/app/recent-projects/recent-projects.component.ts
+++ src/app/recent-projects/recent-projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recent-projects',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recent-projects.component.html',
   styleUrl: './recent-projects.component.scss'
 })
